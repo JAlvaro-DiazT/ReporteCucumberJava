8a33d3d556a1505690873641cabf4b887349a380
JAlvaro-DiazT <jadiazt_1@uqvirtual.edu.co>
2023-10-25 21:55:57 -0500

    Se configura para que saque reportes y al mismo tiempo genere screenshot

--- src/test/java/pom/Hook.java
+++ src/test/java/pom/Hook.java
@@ -2,7 +2,11 @@ package pom;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,22 +16,28 @@ public class Hook {
     private static WebDriver driver;
 
     @Before
-    public void setUp()  {
+    public void setUp(Scenario scenario)  {
         driver = chromeDriverConnection();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("http://localhost:8080");
         driver.manage().window().maximize();
+        takeScreenshotBeforeStep(scenario);
     }
-
+    @After
+    public void tearDown(Scenario scenario){
+        takeScreenshotBeforeStep(scenario);
+        driver.quit();
+    }
     private WebDriver chromeDriverConnection() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         return driver;
     }
 
-    @After
-    public void tearDown(){
-        driver.quit();
+    public void takeScreenshotBeforeStep(Scenario scenario)
+    {
+        byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot,"image/png", "My screenshot step");
     }
     public static WebDriver getDriver() {
         return driver;
--- src/test/java/pom/RunnerTest.java
+++ src/test/java/pom/RunnerTest.java
@@ -2,12 +2,25 @@ package pom;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        glue = {"pom"}
+        glue = {"pom"},
+        plugin = {"json:src/test/resources/reportcucumber/cucumber_report.json"}
 )
 public class RunnerTest {
+    @AfterClass
+    public static void finish(){
+        try {
+            System.out.println("Generar reporte index.html");
+            String[] cmd = {"cmd.exe", "/c", "npm run report"};
+            Runtime.getRuntime().exec(cmd);
+            System.out.println("¡Reporte generado!");
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 }
